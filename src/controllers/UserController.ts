8006a80d9c5f130dc1a4f77cd1e0014688e4cb8d
Fallenefc <arylmoraesn@gmail.com>
2021-10-23 15:21:46 -0600

    register, login, me

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -5,6 +5,7 @@ import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
 import errorHandler from '../utils/errorHandler';
 import { User, UserRole } from '../entities/User';
+import { AuthRequest } from '../middleware/admin';
 
 dotenv.config();
 
@@ -67,6 +68,60 @@ export const createAdmin = async (req: Request, res: Response) => {
                 }
             });
         });
+
+    } catch (error) {
+        return errorHandler(res, 500, 'Server error');
+    }
+}
+
+export const adminLogin = async (req: Request, res: Response) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({where: {username: username, role: UserRole.ADMIN}});
+
+        if (!user) return errorHandler(res, 401, 'Unauthorized.');
+
+        const isValid = await bcrypt.compare(password, user.password);
+
+        if (!isValid) return errorHandler(res, 401, 'Invalid password');
+
+        const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
+            expiresIn: 1000 * 60 * 60 * 24 * 7,
+        });
+
+        return res.status(200).json({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id,
+                user: user.role,
+            },
+            token,
+        })
+
+    } catch (error) {
+        return errorHandler(res, 500, 'Server error');
+    }
+}
+
+export const adminProfile = async (req: AuthRequest, res: Response) => {
+    try {
+        const { user } = req;
+
+        // Just refreshes the duration of the token
+        const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
+            expiresIn: 1000 * 60 * 60 * 24 * 7,
+        });
+
+        return res.status(200).json({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id,
+                role: user.role,
+            },
+            token,
+        })
 
     } catch (error) {
         return errorHandler(res, 500, 'Server error');
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,8 +1,25 @@
 import express from 'express';
-import { createAdmin } from '../controllers/UserController';
+import adminAuthMiddleware from '../middleware/admin';
+import { createAdmin, adminLogin, adminProfile } from '../controllers/UserController';
 
 const router = express.Router();
 
+// GET
+// Get something
+// Does not have body
+
+// POST
+// Request to send something
+
+// PUT
+// Request to update something
+
+// DELETE
+// Request to delete something
+// Does not have body
+
 router.post('/user/createAdmin', createAdmin);
+router.post('/user/admin/login', adminLogin);
+router.get('/user/admin/me', adminAuthMiddleware, adminProfile);
 
 export default router;
